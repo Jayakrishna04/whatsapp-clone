db3d6d48ad3a7ff70362c529791257eb153038dd
Jayakrishna04 <jayakrishnayadav2455@gmail.com>
2024-02-12 13:25:26 +0530

    Updates

---
+++ precache-manifest.c8f9e5c0337c20263cb37006181b2b2c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "851a860f4d61cd098c152022f9ed4352",
+    "url": "/whatsapp-clone/index.html"
+  },
+  {
+    "revision": "a8a06c5b2c3fb67eed3c",
+    "url": "/whatsapp-clone/static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "94ab351ce9721fbedb0a",
+    "url": "/whatsapp-clone/static/css/main.b97c70cc.chunk.css"
+  },
+  {
+    "revision": "a8a06c5b2c3fb67eed3c",
+    "url": "/whatsapp-clone/static/js/2.a418e9db.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/whatsapp-clone/static/js/2.a418e9db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5593ca2cd0934713bb7",
+    "url": "/whatsapp-clone/static/js/3.0409e1cc.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/whatsapp-clone/static/js/3.0409e1cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94ab351ce9721fbedb0a",
+    "url": "/whatsapp-clone/static/js/main.b2225920.chunk.js"
+  },
+  {
+    "revision": "8d82ac571d1ba7f20979",
+    "url": "/whatsapp-clone/static/js/runtime-main.f6d0e998.js"
+  },
+  {
+    "revision": "2d4721273792364bedc17b80d13749df",
+    "url": "/whatsapp-clone/static/media/bg.2d472127.png"
+  }
+]);
